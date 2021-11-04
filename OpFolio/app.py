5ef7310657ce2c57f1f8f219162fba80b425d496
David Brenn <davidbrenn@t-online.de>
2021-11-04 22:11:08 +0100

    Implementation of the basic depot overview

--- OpFolio/app.py
+++ OpFolio/app.py
@@ -2,7 +2,9 @@ from flask import Flask, redirect, url_for, render_template
 from markupsafe import escape
 app = Flask(__name__)
 
-@app.route("/<name>")
-def index(name):
-    return render_template("index.html", content=name)
+@app.route("/")
+def index():
+    return render_template("index.html",stockList=[["MSCI World","20","103,32"],["MSCI EM","10","83,07"]],depotValue=[1003,1000])
 
+if __name__ == "__main__":
+    app.run(debug=True)
