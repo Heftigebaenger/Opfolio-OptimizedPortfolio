701c1198165b6bfe72274bd9d3f3b187b8c4a62f
David Brenn <davidbrenn@t-online.de>
2021-11-05 10:20:20 +0100

    Added base.html for the menu bar and the basis for the stock view

--- OpFolio/app.py
+++ OpFolio/app.py
@@ -6,20 +6,22 @@ depotwertDiff = 0
 kaufwertDiff = 0
 depotwertDiffPro = 0
 kaufwertDiffPro = 0
-stocks = [  ["MSCI World",20,103.32,"R92E92","ETF",0.31,101.01],
-            ["MSCI EM",10,83.07,"A30E59","ETF",0.1,81.01],
-            ["Alibaba",80,19.41,"K10594","Aktie",0.3,67.04]]
+stocks = {  'R92E92':["MSCI World",20,103.32,"R92E92","ETF",0.31,101.01],
+            'A30E59':["MSCI EM",10,83.07,"A30E59","ETF",0.1,81.01],
+            'K10594':["Alibaba",80,19.41,"K10594","Aktie",0.3,67.04]}
 
 def calcDepotwert():
     calc = 0
-    for stock in stocks:
-        calc = calc + stock[1]*stock[2]
+    for key in stocks:
+        calc = calc + stocks[key][1]*stocks[key][2]
+        print()
     return calc
 
 def calcKaufwert():
     calc = 0   
-    for stock in stocks:
-        calc = calc + stock[1]*stock[6]
+    for key in stocks:
+        calc = calc + stocks[key][1]*stocks[key][6]
+        print(stocks[key])
     return calc
     
 
@@ -34,9 +36,17 @@ depotwert = calcDepotwert()
 kaufwert = calcKaufwert()
 kaufwertDiff= calcKaufwertDiff()
 kaufwertDiffPro = calcKaufwertDiffPro()
+
 @app.route("/")
 def index():
     return render_template("index.html",stockList=stocks,depotValue=[depotwert,kaufwert,depotwertDiff,depotwertDiffPro,kaufwertDiff,kaufwertDiffPro])
 
+def getStockPage(key):
+    print(key)
+
+
+@app.route("/stock/<wkn>")
+def stockpage(wkn):
+    return render_template("stock.html",stock=stocks[wkn])
 if __name__ == "__main__":
     app.run(debug=True)
