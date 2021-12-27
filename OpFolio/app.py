f6510b7ba963258e880d70c5613efcc622f728d9
David Brenn <davidbrenn@t-online.de>
2021-12-27 21:11:55 +0100

    Main app now uses Stock class and added TodayData
    Signed-off-by: David Brenn <davidbrenn@t-online.de>

--- OpFolio/app.py
+++ OpFolio/app.py
@@ -2,15 +2,20 @@ from flask import Flask, redirect, url_for, render_template, jsonify
 import requests
 import time, json
 from flask import request
-# Get the app form Flask package
+from stock import Stock 
+
+
 # apikey for alphaventage
 apikeyAlpha = "EBYN9X417QJPRW0H"
 # apikey for ameritrade
 apikeyAmeri = "96I1BNR5FXVTRTMNASI2Z7IPYG07MG9P"
+
+# Get the app form Flask package
 app = Flask(__name__)
 date = str(int(round(time.time() * 1000)))
+print(date);
 # Globalvariable for your Depot
-
+# This needs to be replaced by a SQL database
 depotwert = 0
 kaufwert = 0
 depotwertDiff = 0
@@ -20,6 +25,7 @@ kaufwertDiffPro = 0
 
 # Dictionary with Stocks transactions: 
 # Key = WKN of the Stock, Value = Array[Name,Quantity,Curr. Value,WKN,Type,Difference,Buy Value]
+# This needs to be replaced by a SQL database
 stocks = {  'BNTX':["BionTech",20,103.32,"BNTX","Aktie",0.31,101.01],
             'AAPL':["Apple",10,83.07,"AAPL","Aktie",0.1,81.01],
             'BABA':["Alibaba",80,19.41,"BABA","Aktie",0.3,67.04]}
@@ -67,24 +73,18 @@ def index():
 def stockpage(wkn):
     #Renders the stock.html with these arguments: 
     # stock = Array[Name,Quantity,Curr. Value,WKN,Type,Difference,Buy Value]
-    return render_template("stock.html",stock=stocks[wkn])
+    return render_template("stock.html")
 
 @app.route("/stock/api/<symbol>", methods=["GET"])
 def apidata(symbol):
-    apiStockData = {
-        "info":{},
-        "lastMonth":{},
-        "quotes": {}
-    }
-    # url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=&apikey='+apikey
-    # url = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=apple&apikey=" + apikey
-
+    
+    stockData = Stock(symbol,{},{},{})
+    stockData.lastMonth = apiAmeriLastMonthData(symbol)
+    stockData.informations = apiAmeriQuotes(symbol)
+    stockData.today = apiAmeriToday(symbol)
     symbol = request.headers.get("symbol")
     # url = "https://www.alphavantage.co/query?function=OVERVIEW&symbol="+symbol+"&apikey=" + apikey
-    apiStockData["info"] = apiAmeriFundamental(symbol)
-    apiStockData["lastMonth"] = apiAmeriLastMonthData(symbol)
-    apiStockData["quotes"] = apiAmeriQuotes(symbol)
-    return json.dumps(apiStockData)
+    return json.dumps(stockData.__dict__)
 # Runs the app 
 
 def apiAmeriFundamental(symbol):
@@ -96,6 +96,13 @@ def apiAmeriFundamental(symbol):
 
 def apiAmeriLastMonthData(symbol):
     url = "https://api.tdameritrade.com/v1/marketdata/"+symbol+"/pricehistory?apikey="+apikeyAmeri+"&periodType=month&period=1&frequencyType=daily&frequency=1&endDate="+date+"&needExtendedHoursData=true"
+    r = requests.get(url)
+    data = r.json()
+    return data
+
+def apiAmeriToday(symbol):
+    date = str(int(round(time.time() * 1000)))
+    url = "https://api.tdameritrade.com/v1/marketdata/"+symbol+"/pricehistory?apikey="+apikeyAmeri+"&periodType=day&period=1&frequencyType=minute&frequency=5&endDate="+date+"&needExtendedHoursData=false"
     r = requests.get(url)
     data = r.json()
     return data
