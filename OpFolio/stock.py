a00391b5ee09b471837b967815c751f984f8f5eb
David Brenn <davidbrenn@t-online.de>
2022-01-02 21:33:40 +0100

    Added last6Month to Stock class
    Signed-off-by: David Brenn <davidbrenn@t-online.de>

--- OpFolio/stock.py
+++ OpFolio/stock.py
@@ -1,12 +1,14 @@
 class Stock():
     symbol = ""
+    last6Month = {}
     lastMonth = {}
     today = {}
     informations = {}
 
-    def __init__(self,symbol,informations,lastMonth,today):
+    def __init__(self,symbol,informations,lastMonth,today,last6Month):
         self.symbol = symbol
         self.informations = informations
         self.lastMonth = lastMonth
         self.today = today
+        self.last6Month = last6Month
 
