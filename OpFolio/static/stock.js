eb0e19097d0fd58d47f849525a87051ba8b09a6e
David Brenn <davidbrenn@t-online.de>
2022-01-02 21:35:52 +0100

    New stucture for Stockview + cleaned stock.js
    Signed-off-by: David Brenn <davidbrenn@t-online.de>

--- OpFolio/static/stock.js
+++ OpFolio/static/stock.js
@@ -11,26 +11,34 @@ fetch("/stock/api/"+id,{headers: {'symbol': id}}).then(response => response.json
 
 
 function useData(stockJson){
+    
     console.log(stockJson); 
-    var para = document.createElement("p");
-    var node = document.createTextNode(stockJson["informations"]["description"]);
-    para.appendChild(node);
-    var element = document.getElementById("mainDiv");
-    element.appendChild(para);
 
-    para = document.createElement("p");
-    node = document.createTextNode(stockJson["informations"]["regularMarketLastPrice"]);
-    para.appendChild(node);
-    element = document.getElementById("mainDiv");
-    element.appendChild(para);
+    addContent("h2",stockJson["informations"]["description"]+" ("+id+")","stockNameDiv");
+    addContent("p",stockJson["informations"]["exchangeName"],"stockNameDiv",null,"smallP");
+    addContent("span",stockJson["informations"]["regularMarketLastPrice"],"stockPriceDiv","stockPrice");
 
-    para = document.createElement("p");
-    var gains = parseFloat(parseFloat(stockJson["informations"]["regularMarketLastPrice"]) - parseFloat(stockJson["lastMonth"]["candles"][stockJson["lastMonth"]["candles"].length-2]["close"])).toFixed(2)
-    node = document.createTextNode(gains);
-    para.appendChild(node);
-    element = document.getElementById("mainDiv");
-    element.appendChild(para);
+    var gains = parseFloat(stockJson["informations"]["regularMarketNetChange"]);
+    var percentGains = parseFloat(stockJson["informations"]["regularMarketPercentChangeInDouble"]).toFixed(2);
+    addContent("span",gains+" ("+percentGains+"%)","stockPriceDiv","stockGains");
+    if (gains < 0){
+        document.getElementById("stockGains").style.color = "red"
+    } else document.getElementById("stockGains").style.color = "greenyellow"
 
+    let stockDate = new Date(parseInt(stockJson["informations"]["regularMarketTradeTimeInLong"]))
+    let stockMinutes = stockDate.getMinutes();
+    let stockHours = stockDate.getHours();
+    if(stockDate.getMinutes() <= 9){
+        stockMinutes = "0"+stockMinutes;
+    }
+    if(stockDate.getHours() <= 9){
+        stockHours = "0"+stockHours;
+    }
+    
+    addContent("p","Last updated "+stockHours+":"+stockMinutes+", "+stockDate.getDate()+"."+month[stockDate.getMonth()],"stockPriceDiv",null,"smallP");
+
+    addContent("p","Volumen: "+stockJson["informations"]["totalVolume"],"stockInfoDiv");
+    
 
     const labelsLastMonth = [];
     const labelsToday = []
@@ -81,8 +89,8 @@ function useData(stockJson){
     element.appendChild(buttonToday);
 
     buttonToday.addEventListener("click", () => {
-        chart.data = dataToday
-        chart.update()
+        chart.data = dataToday;
+        chart.update();
     });
 
     var buttonLastMonth = document.createElement("button");
@@ -91,10 +99,19 @@ function useData(stockJson){
     element.appendChild(buttonLastMonth);
 
     buttonLastMonth.addEventListener("click", () => {
-        chart.data = lastMonthData
-        chart.update()
+        chart.data = lastMonthData;
+        chart.update();
     });
 
+    var buttonLast6Month = document.createElement("button");
+    var buttonLast6MonthText = document.createTextNode("Last 6 Month");
+    buttonLast6Month.append(buttonLast6MonthText);
+    element.appendChild(buttonLast6Month);
+
+    buttonLast6Month.addEventListener("click", () => {
+        chart.data = last6MonthData;
+        chart.update();
+    });
     
 
 
@@ -113,6 +130,41 @@ function useData(stockJson){
           data: stockLastMonthData,
         }]
     };
+
+    const labelsLast6Month = [];
+    const stockLast6MonthData = [];
+
+    stockJson["last6Month"]["candles"].forEach(candles => {
+        stockLast6MonthData.push(candles["close"]);
+        let date = new Date(candles["datetime"]);
+        labelsLast6Month.push(date.getDate()+" "+month[date.getMonth()]);
+    });
+    
+    const last6MonthData = {
+        labels: labelsLast6Month,
+        datasets: [{
+            backgroundColor: 'rgb(255, 150, 69)',
+            borderColor: 'rgb(255,150,69)',
+            data: stockLast6MonthData,
+        }]
+    }
+}
+
+//Creats a html element of tpye "elementType" with the text "elementText" and binds it to "parentElement"
+function addContent(elementType,elementText,parentElement,idName = null,className = null){
+    var para = document.createElement(elementType);
+    var node = document.createTextNode(elementText);
+    para.appendChild(node);
+    
+    if(idName != null){
+       para.id = idName;
+    }
+    if(className != null){
+        para.classList.add("smallP");
+    }
+
+    var element = document.getElementById(parentElement);
+    element.appendChild(para);
 }
 
 
