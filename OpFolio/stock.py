892ea8273702e97feb9883eb153bf7ab97a33304
David Brenn <davidbrenn@t-online.de>
2022-04-26 17:05:40 +0200

    Merge
    Signed-off-by: David Brenn <davidbrenn@t-online.de>

--- OpFolio/stock.py
+++ OpFolio/stock.py
@@ -6,21 +6,15 @@ from logging import error, raiseExceptions
 from math import sqrt
 from statistics import variance
 from time import time
-<<<<<<< HEAD
 from datetime import datetime
 from typing import Final, final
 
 # Stock Class to uniform Stockdata
-=======
-
-
->>>>>>> b29bb43c4622431c8b0337ee0ee4f44eae54612d
 class Stock():
     tradingdays: Final = 253
     #Basic Information
     symbol = ""
     informations = {}
-<<<<<<< HEAD
     companyInfo = {}
     
     #Stock price data
@@ -42,18 +36,6 @@ class Stock():
     last5YearsDailyYields = []
 
     def __init__(self,symbol,informations,lastMonth,today,last6Month,lastYear,companyInfo,last5Years,last5YearsDaily):
-=======
-    lastYear = {}
-    companyInfo = {}
-    last5Years = {}
-    yieldLast5Years = {}
-    standartDeviation = 0
-    averageYield = 0
-    varianz = 0
-    standartDevi = 0
-
-    def __init__(self,symbol,informations,lastMonth,today,last6Month,lastYear, companyInfo,last5Years):
->>>>>>> b29bb43c4622431c8b0337ee0ee4f44eae54612d
         self.symbol = symbol
         self.informations = informations
         self.lastMonth = lastMonth
@@ -62,7 +44,6 @@ class Stock():
         self.lastYear = lastYear
         self.companyInfo = companyInfo
         self.last5Years = last5Years
-<<<<<<< HEAD
         self.yieldForEachYear = [0,0,0,0,0]
         self.yields = []
         self.yieldMonthly = []
@@ -139,80 +120,9 @@ class Stock():
 
 
     def calcAverageYield(self,arrayOfYields):
-=======
-        self.yields = []
-    def calc(self):
-
-        self.yields = self.calcYields(60)
-        self.averageYield = self.calcAverageYield(self.yields[1])
-        self.varianz = self.calcVarianz()
-        self.standartDevi = self.calcStandartDevi()
-
-    def calcVarianz(self):
-        if len(self.yields[1]) <= 0:
-            error("No AverageYields")
-        sum = 0
-        for yields in self.yields[1]:
-            sum = sum + (yields - self.averageYield)**2
-        sum = sum / len(self.yields[1])
-        return sum
-
-    def calcStandartDevi(self):
-        if(self.varianz <= 0):
-            error("Varianz = 0")
-        return sqrt(self.varianz)
-
-    def calcYields(self,months):
-        if(months < 12):
-            error("calcYields: Less then 6 Months")
-        years = int(months / 12)
-        print("Years:")
-        print(years)
-        lastMonthIndex = len(self.last5Years["candles"]) - 1
-        averageMonthlyYield = []
-        averageYearlyYield = []
-        for year in range(years):
-            startMonthIndex = lastMonthIndex - year * 12
-            endMonthIndex = startMonthIndex - 12
-            averageMonthlyYieldForOneYear = self.calcMonthlyYieldAverage(startMonthIndex,endMonthIndex, self.last5Years["candles"])
-            averageMonthlyYield.append(averageMonthlyYieldForOneYear)
-            averageYearlyYield.append((1+averageMonthlyYieldForOneYear)**12-1)
-            print(averageMonthlyYield)
-        return [averageMonthlyYield,averageYearlyYield]
-
-        
-
-    def calcMonthlyYieldAverage(self, startIndex, endIndex, candleArray):
-        if(startIndex < 0):
-            error("calcMonthly: startIndex < 0")
-        if(endIndex > startIndex or endIndex < 0):
-            error("calcMonthly: endIndex wrong")
-        monthlyYields = []
-        print(endIndex)
-        while (startIndex > endIndex and startIndex >0):
-            print("StartIndex:")
-            print(startIndex)
-            yieldForOneMonth = (candleArray[startIndex]["close"] - candleArray[startIndex -1]["close"]) / candleArray[startIndex - 1]["close"]
-            monthlyYields.append(yieldForOneMonth)
-            startIndex = startIndex - 1 
-        print("Monthly Yields")
-        print (monthlyYields)
-        return self.calcAverageYield(monthlyYields)
-
-
-    def calcYieldMonthlytoYear(self):
-        self.yieldLast5Years 
-
-
-
-    def calcAverageYield(self, arrayOfYields):
->>>>>>> b29bb43c4622431c8b0337ee0ee4f44eae54612d
         averageYield = 0
         for yields in arrayOfYields:
             averageYield = averageYield + yields
         averageYield = averageYield / len(arrayOfYields)
-<<<<<<< HEAD
         print("Length of Avrage Array: "+str(len(arrayOfYields)))
-=======
->>>>>>> b29bb43c4622431c8b0337ee0ee4f44eae54612d
         return averageYield
