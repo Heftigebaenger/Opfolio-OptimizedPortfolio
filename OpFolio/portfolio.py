c90d50c708057bde7d9fc74ffde078e9d9d7d135
David Brenn <davidbrenn@t-online.de>
2022-08-22 23:46:23 +0200

    update multiProfolio()
    Signed-off-by: David Brenn <davidbrenn@t-online.de>

--- OpFolio/portfolio.py
+++ OpFolio/portfolio.py
@@ -1,3 +1,5 @@
+from audioop import mul
+from distutils.spawn import find_executable
 from logging import error
 from operator import truediv
 from yfinanceAPI import YahooAPI
@@ -12,7 +14,7 @@ class Portfolio():
     id = 0
     def __init__(self,id):
         self.id = id
-        self.loadFromFile()
+        #self.loadFromFile()
 
     def addStock(self,symbol,shares):
         self.stockList.append((symbol,shares))
@@ -56,27 +58,66 @@ class Portfolio():
         print(KovMatrix)
 
     # ToDo: Ist aktuell noch nichts berechnet 
-    def CorrMatrix():
-        stocks = ["MSFT","AAPL","BNTX"]
+    def CorrMatrix(self,stocks):
         data_stocks = pd.DataFrame()
         for symbol in stocks:
             data_stocks[symbol]=YahooAPI.getOneYearYields(symbol, lg=True)
-        KovMatrix = data_stocks.corr()
-        print(KovMatrix)
+        CorrMatrix = data_stocks.corr()
+        print(CorrMatrix)
+        return CorrMatrix
 
 
-    def multiPortfolio():
-        stocks = ["MSFT","AAPL","BNTX","Test"]
-        rndm_nr = np.empty()
-        port_shares = np.empty(0)
-        i = 0
-        while i < len(stocks):
-            np.append(rndm_nr, random.random())
-            i += 1
-        print(rndm_nr)
+    def multiPortfolio(self):
+        stocks = ["MSFT","AAPL","BNTX","SAP"]
+        yields = [0.2,0.6,0.3,0.4]
+        #rndm_nr = np.empty()
+        #port_shares = np.empty(0)
+        #i = 0
+        #while i < len(stocks):
+         #   np.append(rndm_nr, random.random())
+          #  i += 1
+        #print(rndm_nr)
+
+
+        randomMatrix = np.random.rand(500,len(stocks))
+        sumRandomMatrix = np.sum(randomMatrix, axis=1)
+        divisionMatix = np.tile(sumRandomMatrix, (len(stocks),1))
+        anteilMatrix = randomMatrix / divisionMatix.T
+        print(randomMatrix)
+        print(sumRandomMatrix.T)
+        print(divisionMatix.T)
+        print("Final Matrix")
+        print(anteilMatrix)
+        print("Sum Final Matrix")
+
+        #yieldsBigMatrix = np.tile(yields, (500,1))
+        #print("Yield Matrix")
+        #print(yieldsBigMatrix.T)
+        portfolioRenditeVector = np.dot(anteilMatrix,yields)
+        
+        corrMatrix = np.array(self.CorrMatrix(stocks))
+        print(corrMatrix)
+        
+        riskMatrix = np.apply_along_axis(self.multiPortfolioHelper,1,corrMatrix,anteilMatrix)
+        print(riskMatrix)
+        #print(portfolioRenditeVector)
+
+    def multiPortfolioHelper(self,corrMatrixVector,anteilMatrix):
+        print(corrMatrixVector)
+        return np.dot(anteilMatrix,corrMatrixVector)
 
 
 
-    multiPortfolio()
+portfolioOne = Portfolio(1)
+portfolioOne.multiPortfolio()
+
+  
+
+
+
+
+
+
+
     
         
